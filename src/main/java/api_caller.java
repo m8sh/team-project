155cdfecdf3fb120b3e02e5cb01022c80c19e186
will <vanish25th@gmail.com>
2025-11-12 14:49:46 -0500

    added for "functionality"

--- src/main/java/api_caller.java
+++ src/main/java/api_caller.java
@@ -6,40 +6,59 @@ import java.net.URL;
 
 public class api_caller {
 
-
     private URL urlBase = new URL("http://shrill-forest-40bb.sw-william08.workers.dev");
 
     public api_caller() throws MalformedURLException {
     }
 
     public static void main(String[] args) throws MalformedURLException {
-
+        System.out.println("API Caller Started");
     }
-
+    // Pre: pin is a String
+    // Post:
     public void createRoom(String pin) throws MalformedURLException {
-        URL createNewRoomURL = new URL((this.urlBase + "/"));
+        System.out.println("Creating Room");
     }
 
+    // Pre:
+    // Post:
     public void joinRoom(String pin, String username){
+        System.out.println("Joining Room");
 
     }
 
+    // Pre:
+    // Post:
     public void endGame(String pin){
-    }
-
-    public void sendQuestions(String pin, String[] questionList){
+        System.out.println("Ending Game");
 
     }
 
+    // Pre:
+    // Post:
+    public void sendQuestions(String pin, Object[] questionList){
+        System.out.println("Sending Questions");
+
+    }
+
+    // Pre:
+    // Post:
     public void recieveQuestions(String pin){
+        System.out.println("Recieving Questions");
 
     }
 
-    public void sendAnswers(String pin, String[] answerList){
+    // Pre:
+    // Post:
+    public void sendAnswers(String pin, Object[] answerList){
+        System.out.println("Sending Answers");
 
     }
 
+    // Pre:
+    // Post:
     public void recieveAnswers(String pin){
+        System.out.println("Recieving Answers");
 
     }
 }
