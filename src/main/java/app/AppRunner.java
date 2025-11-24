39784f2044e6e73d970d758713e64dff0d74e6fb
xande <xandersaiki@gmail.com>
2025-11-24 10:40:04 -0500

    Implemented use case of adding a question to a lobby. Added tests to assess the AddQuestion interactor and all use case classes. Added a LobbyPrep View and an addQuestionView.

---
+++ src/main/java/app/AppRunner.java
@@ -0,0 +1,15 @@
+package app;
+
+import use_cases.view.LobbyPrepView;
+import interfaceAdapters.AddQuestion.*;
+
+import use_cases.AddQuestion.AddQuestionInteractor;
+
+import javax.swing.*;
+
+public class AppBuilder {
+    //Go through each use case, set everything up
+
+
+    
+}
