bef6960b8bb68a6727fbbe33f8a5c19029acef81
will <vanish25th@gmail.com>
2025-11-26 10:20:03 -0500

    WORKING (hopefully) API CALLER

--- src/main/java/api_caller.java
+++ src/main/java/api_caller.java
@@ -7,7 +7,9 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.URL;
 import java.net.http.WebSocket;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
@@ -18,8 +20,8 @@ public class api_caller {
     private final String wsBase = "wss://shrill-forest-40bb.sw-william08.workers.dev";
     private final HttpClient http = HttpClient.newHttpClient();
     private final Map<String, WebSocket> sockets =  new HashMap<>();
-    private Object[] receivedQuestions = null;
-    private Object[] receivedAnswers = null;
+    private static Object[] receivedQuestions = null;
+    private static Object[] receivedAnswers = null;
 
     public api_caller() throws MalformedURLException {
     }
@@ -66,17 +68,31 @@ public class api_caller {
     // Pre: make the questionList contain an array of arrays, that contains a bunch of questions
     // For example, ["how many sides in a square?", "what is the color of the sun?", etc.]
     // Post: Sends those questions to the server
-    public void sendQuestions(String pin, Object[] questionList){
+    public void sendQuestions(String pin, Question[] questionList){
         System.out.println("Sending Questions");
         WebSocket ws = sockets.get(pin);
         StringBuilder questionsListString = new StringBuilder();
         questionsListString.append("[");
+
         for (int i = 0; i < questionList.length; i++){
-            questionsListString.append('"').append(questionList[i].toString()).append('"');
-            if (i + 1 < questionList.length){
-                questionsListString.append(",");
+            Question question = questionList[i];
+            if (i > 0) questionsListString.append(",");
+
+            questionsListString.append("{\"prompt\":\"");
+            questionsListString.append(question.getPrompt());
+            questionsListString.append("\",\"choices\":[");
+            ArrayList<String> choices = question.getChoices();
+            for (int j = 0; j < choices.size(); j++){
+                if (j > 0) questionsListString.append(",");
+                questionsListString.append("\"" + choices.get(j) + "\"");
             }
+            questionsListString.append("]");
+
+            questionsListString.append("\"correctIndex\":" + question.getCorrectIndex() + "}");
+
+            questionsListString.append('"').append(questionList[i].toString()).append('"');
         }
+
         questionsListString.append("]");
 
         ws.sendText("{\"type\":\"host/sendQuestions\",\"questions\":" + questionsListString.toString() + "}", true).join();
@@ -87,6 +103,8 @@ public class api_caller {
     public Object[] recieveQuestions(String pin){
         System.out.println("Recieving Questions");
         WebSocket ws = sockets.get(pin);
+
+        receivedQuestions = null;
 
         ws.sendText("{\"type\":\"client/receiveQuestions\"}", true).join();
 
@@ -124,6 +142,8 @@ public class api_caller {
         System.out.println("Recieving Answers");
         WebSocket ws = sockets.get(pin);
 
+        receivedAnswers = null;
+
         ws.sendText("{\"type\":\"host/receiveAnswers\"}", true).join();
 
         while (receivedAnswers == null){
@@ -149,19 +169,90 @@ public class api_caller {
         @Override
         public void onOpen(WebSocket webSocket) {
             WebSocket.Listener.super.onOpen(webSocket);
+            System.out.println("opened");
         }
         @Override
         public CompletableFuture<?> onText(WebSocket webSocket, CharSequence data, boolean last){
+            System.out.println("text received");
+            String string = data.toString();
+
+            if (string.contains("\"type\":\"questions\"")){
+                receivedQuestions = extractJSON(string, "\"questions\"");
+            }
+
+            if (string.contains("\"type\":\"client/answers\"")){
+                receivedAnswers = extractJSON(string, "\"answers\"");
+            }
+
             return WebSocket.Listener.super.onText(webSocket,data,last).toCompletableFuture();
         }
         @Override
         public void onError(WebSocket webSocket, Throwable t){
             WebSocket.Listener.super.onError(webSocket,t);
+            System.out.println("error");
         }
         @Override
         public CompletableFuture<?> onClose(WebSocket webSocket, int statusCode, String reason){
+            System.out.println("closed");
             return WebSocket.Listener.super.onClose(webSocket,statusCode,reason).toCompletableFuture();
         }
+    }
+
+    private static Object[] extractJSON(String json, String key){
+        int index = json.indexOf(key);
+        int depth = 0;
+        int start = -1;
+        boolean inString = false;
+        char previous = '0';
+
+        if (index < 0) return new Object[0];
+
+        int arrStartIndex = json.indexOf('[', index);
+        if (arrStartIndex < 0) return new Object[0];
+
+        depth = 1;
+        int arrEndIndex = -1;
+        for (int i = arrStartIndex + 1; i < json.length(); i++){
+            char ch = json.charAt(i);
+            if (ch == '"' && previous != '\\') inString = !inString;
+            if (!inString){
+                if (ch == '[') depth++;
+                else if (ch == '{') {
+                    depth++;
+                    if (depth == 0){
+                        arrEndIndex = i;
+                        break;
+                    }
+                }
+
+            }
+            previous = ch;
+        }
+
+        String body = json.substring(arrStartIndex + 1, arrEndIndex);
+        if (body.isEmpty()) return new Object[0];
+
+        List<String> out = new ArrayList<>();
+        for (int i = 0; i < body.length(); i++){
+            char ch = body.charAt(i);
+            if (ch == '"' && previous != '\\') inString = !inString;
+            if (!inString){
+                if (ch == '{'){
+                    if (depth == 0){
+                        start = i;
+                        depth++;
+                    }
+                    else if  (ch == '}') {
+                        depth--;
+                        if (depth == 0) {
+                            out.add(body.substring(start, i+1));
+                        }
+                    }
+                }
+            }
+            previous = body.charAt(i);
+        }
+        return out.toArray();
     }
 
 }
